de30737e286e839f9adb6121f2a114c29397c4e6
akshaysangma <akshay.sangma@outlook.com>
2023-12-02 01:46:24 +0530

    add day 1 part 2

--- day01/main.go
+++ day01/main.go
@@ -7,10 +7,25 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+var conversions = []string{
+	"one", "o1e",
+	"two", "t2o",
+	"three", "t3e",
+	"four", "f4r",
+	"five", "f5e",
+	"six", "s6x",
+	"seven", "s7n",
+	"eight", "e8t",
+	"nine", "n9e",
+	"zero", "z0o",
+}
 
 func main() {
 	fmt.Println(partOne())
+	fmt.Println(partTwo())
 }
 
 func partOne() string {
@@ -33,5 +48,36 @@ func partOne() string {
 		}
 	}
 
-	return fmt.Sprintln("Answer to part one is ", total)
+	return fmt.Sprint("Answer to part one is ", total)
+}
+
+// sadly (?=(one|two|three|four|five|six|seven|eight|nine|zero|\d)) povitive lookahead is not supported
+// in go. Hence unable to handle overlapping kinds like oneight (1,8). Refer https://github.com/google/re2/wiki/Syntax
+func partTwo() string {
+	input1, err := os.Open("input1.txt")
+	if err != nil {
+		return fmt.Sprintf("unable to read input file : %s", err)
+	}
+	re := regexp.MustCompile(`\d`)
+	scanner := bufio.NewScanner(input1)
+	var total int
+
+	for scanner.Scan() {
+		parser := strings.NewReplacer(conversions...)
+
+		// Since string.NewReplacer performs non-overlapping replacement
+		// performing replacement twice will be able to handle overlapping
+		// conversion from digit  word to digit as long as we maintain
+		// first and last char of the digit word to enable secord replacement.
+		parsed := parser.Replace(parser.Replace(scanner.Text()))
+
+		if digits := re.FindAllString(parsed, -1); digits != nil {
+			twoDigit := digits[0] + digits[len(digits)-1]
+			// Can ignore error as regex guarantees digits are ints
+			num, _ := strconv.Atoi(twoDigit)
+			total += num
+		}
+	}
+
+	return fmt.Sprint("Answer to part two is ", total)
 }
